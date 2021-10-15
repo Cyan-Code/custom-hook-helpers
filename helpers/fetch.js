2f56b4b4dab43582dbd3c252d5dba8579f9aa035
Luis Carrion <60019302+Cyan-Code@users.noreply.github.com>
2021-10-14 20:21:19 -0500

    Helpers and Fetch added

---
+++ helpers/fetch.js
@@ -0,0 +1,42 @@
+const baseURL = process.env.REACT_APP_API_URL // P/Dev/test
+
+export const fetchSinToken = ( endPoint, data, method='GET' ) => {
+  const url = `${ baseURL }/${ endPoint }`
+  if(method === 'GET') {
+    return fetch(url)
+  } else {
+    return fetch( url, {
+      method,
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify( data )
+    })
+  }
+}
+
+export const fetchConToken = async (endpoint, data, method = 'GET') => {
+
+  const url = `${baseURL}/${endpoint}`;
+  const token = localStorage.getItem('token') || '';
+  
+  if (method === 'GET') {
+    const resp = await fetch(url, {
+      headers: {
+        'x-token': token
+      }
+    });
+    return await resp.json();
+  } else {
+    const resp = await fetch(url, {
+      method,
+      headers: {
+        'Content-type': 'application/json',
+        'x-token': token
+      },
+      body: JSON.stringify(data)
+    })
+
+    return await resp.json();
+  }
+}
